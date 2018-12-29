197889e32302a5fee430ade2dd384e04b17776b8
calvi mondele <cmondele88@gmail.com>
2018-12-29 15:41:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Bill Nye";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "hello" + specialGuest + "!"
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `hello, ${specialGuest}` + "!";
+
+
